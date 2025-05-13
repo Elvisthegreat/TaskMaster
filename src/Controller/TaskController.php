45beb98c3fc7a534d23f8b87d2ce4478c0f25aca
Elvisthegreat <amadinelvis6@gmail.com>
2025-05-13 06:02:00 +0200

    Add new functionality to Taskcontroller to view individual task and a new template

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\TaskmasterRepository;
+use App\Entity\Taskmaster;
 
 class TaskController extends AbstractController
 {
@@ -15,6 +16,14 @@ class TaskController extends AbstractController
         // Fetch all tasks from the repository
         return $this->render('/task/list.html.twig', [
             'tasks' => $repository->findAll(),
+        ]);
+    }
+
+    #[Route('/task/{id}', name: 'task_show')]
+    public function show(Taskmaster $task): Response
+    {
+        return $this->render('/task/show.html.twig', [
+            'task' => $task
         ]);
     }
 }
